6818a0f0adffa76b6715fa451683a5ccc68aeb65
1009394958 <76431457+1009394958@users.noreply.github.com>
2026-06-21 20:38:41 +0800

    Fix - 标准量子X1.6.0格式 body变两行

--- ywld_poem.js
+++ ywld_poem.js
@@ -1,4 +1,5 @@
-var obj = JSON.parse($response.body);
+var body = $response.body;
+var obj = JSON.parse(body);
 obj.data.summary.limit_score = 0;
 obj.data.summary.over_count = 999;
 obj.data.summary.total_count = 999;
