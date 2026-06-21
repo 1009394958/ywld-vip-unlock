529ec58e4e181ce7b01610c6361367d483a7eae0
1009394958 <76431457+1009394958@users.noreply.github.com>
2026-06-21 20:38:39 +0800

    Fix - 标准量子X1.6.0格式 body变两行

--- ywld_user.js
+++ ywld_user.js
@@ -1,5 +1,5 @@
-var obj = $response.body;
-if (typeof obj === "string") obj = JSON.parse(obj);
+var body = $response.body;
+var obj = JSON.parse(body);
 obj.data.is_vip = 1;
 obj.data.vip_expire_date = "2099-12-31";
 obj.data.vip_type = 2;
