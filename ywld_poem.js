29eefb4c28f36d4d0f51d5e2397847d47b9c1be2
1009394958 <76431457+1009394958@users.noreply.github.com>
2026-06-21 20:33:21 +0800

    Fix - 极简量子X标准格式

--- ywld_poem.js
+++ ywld_poem.js
@@ -1,5 +1,5 @@
-let body = $response.body;
-body = body.replace(/"limit_score":\d+/g, '"limit_score":0');
-body = body.replace(/"over_count":\d+/g, '"over_count":999');
-body = body.replace(/"total_count":\d+/g, '"total_count":999');
-$done({body});
+var obj = JSON.parse($response.body);
+obj.data.summary.limit_score = 0;
+obj.data.summary.over_count = 999;
+obj.data.summary.total_count = 999;
+$done({body: JSON.stringify(obj)});
