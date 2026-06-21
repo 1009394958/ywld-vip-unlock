0a22863e6870e132b8c9f5a5dc36b21356cc0008
1009394958 <1009394958@users.noreply.github.com>
2026-06-21 20:16:22 +0800

    Fix - 子资源标准量子X格式

--- ywld_poem.js
+++ ywld_poem.js
@@ -1,10 +1,7 @@
-
-var obj = JSON.parse($response.body);
-
+let obj = JSON.parse($response.body);
 if (obj.data && obj.data.summary) {
   obj.data.summary.limit_score = 0;
   obj.data.summary.over_count = 999;
   obj.data.summary.total_count = 999;
 }
-
 $done({body: JSON.stringify(obj)});
