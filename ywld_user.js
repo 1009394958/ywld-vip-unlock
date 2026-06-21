88440178eed5bf33972be46320db70baec4e7f4c
1009394958 <76431457+1009394958@users.noreply.github.com>
2026-06-21 20:22:38 +0800

    Fix - 删除sign校验+安全body解析

--- ywld_user.js
+++ ywld_user.js
@@ -1,9 +1,12 @@
-let obj = JSON.parse($response.body);
-obj.data.is_vip = 1;
-obj.data.vip_expire_date = "2099-12-31";
-obj.data.vip_type = 2;
-obj.data.is_paid = 1;
-obj.data.has_ebook = 1;
-obj.data.is_teacher = 0;
-obj.data.is_student = 0;
+let obj = JSON.parse(typeof $response.body === "string" ? $response.body : JSON.stringify($response.body));
+if (obj.data) {
+  obj.data.is_vip = 1;
+  obj.data.vip_expire_date = "2099-12-31";
+  obj.data.vip_type = 2;
+  obj.data.is_paid = 1;
+  obj.data.has_ebook = 1;
+  obj.data.is_teacher = 0;
+  obj.data.is_student = 0;
+}
+delete obj.sign;
 $done({body: JSON.stringify(obj)});
